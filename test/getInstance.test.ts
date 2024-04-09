27d50a9c8d5d4b7a126c2f59e0e1520bda5214b1
samdsk <sam.ds@live.com>
2024-04-09 18:16:57 +0200

    bearer token renew test

--- test/getInstance.test.ts
+++ test/getInstance.test.ts
@@ -5,4 +5,16 @@ describe("AxiosGetInstance", () => {
         const instance = await getInstance("sam", "password");
         expect(instance.defaults.headers.Authorization).toMatch(/^Bearer/)
     })
-})
+});
+
+describe("AxiosRequestForNewTokenWhenExpires", () => {
+    it("axios should ask for a new bearer token when expires current one", async () => {
+        const instance = await getInstance("sam", "password");
+        const oldToken = instance.defaults.headers.Authorization;
+
+        await sleep(6000);        
+        await instance.get("/projects");
+
+        expect(instance.defaults.headers.Authentication).not.toEqual(oldToken);
+    },7000)
+});
